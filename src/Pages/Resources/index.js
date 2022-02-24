881d0522ab0adba166317eb6d1b2d1565a4bc23a
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-24 11:58:30 +0000

    added authentication checks

--- src/Pages/Resources/index.js
+++ src/Pages/Resources/index.js
@@ -1,14 +1,31 @@
 import Profile from "../../Components/Profile";
 import LogoutButton from "../../Components/LogoutButton";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Resources() {
-  return (
+  const { isLoading, isAuthenticated, logout } = useAuth0();
+
+  if (!isLoading && !isAuthenticated) {
+    logout({ returnTo: window.location.origin });
+  }
+
+  return isAuthenticated ? (
     <>
+      <nav>
+        <Link to="/add-new">Add new</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/update">Update</Link>
+      </nav>
       <main>
-        <h2>Welcome to the Resources page!</h2>
+        <h2>Welcome to the resources!</h2>
       </main>
       <Profile />
       <LogoutButton />
     </>
+  ) : (
+    <div>
+      <h1>Loading...</h1>
+    </div>
   );
 }
