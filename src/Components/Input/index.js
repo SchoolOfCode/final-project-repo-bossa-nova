cf6c0a3b85f80b60186489cf3f67780b1fa7f474
kattana <kattanaluz@gmail.com>
2022-02-25 16:14:37 +0000

    moved  the useReducer to AddNew Page

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,98 +1,17 @@
-import React, { useReducer } from "react";
+import React from "react";
+const inputStyle = { outline: "solid" };
 
-const initialValues = {
-  jobTitle: "",
-  company: "",
-  salary: "",
-  jobStatus: "",
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "update_input":
-      return {
-        ...state,
-        [action.key]: action.value,
-      };
-    default:
-      return state;
-  }
-}
-
-export default function Input() {
-  const [state, dispatch] = useReducer(reducer, initialValues);
-
-  console.log(state)
-
+export default function Input({ labelText, type, name, value, update }) {
   return (
-    <form>
-      <label>
-        Job Title
-        <input
-          type="text"
-          name="jobTitle"
-          value={state.value}
-          onChange={(e) => dispatch({
-            type: 'update_input',
-            value: e.target.value,
-            key: 'jobTitle'
-          })}
-        />
-      </label>
-      <label>
-        Company
-        <input
-          type="text"
-          name="company"
-          value={state.value}
-          onChange={(e) => dispatch({
-            type: 'update_input',
-            value: e.target.value,
-            key: 'company'
-          })}
-        />
-      </label>
-      <label>
-        Salary
-        <input
-          type="text"
-          name="salary"
-          value={state.value}
-          onChange={(e) => dispatch({
-            type: 'update_input',
-            value: e.target.value,
-            key: 'salary'
-          })}
-        />
-      </label>
-      <label>
-        Job Status
-        <input
-          type="text"
-          name="jobStatus"
-          value={state.value}
-          onChange={(e) => dispatch({
-            type: 'update_input',
-            value: e.target.value,
-            key: 'jobStatus'
-          })}
-        />
-      </label>
-    </form>
+    <label>
+      {labelText}
+      <input
+        style={inputStyle}
+        type={type}
+        name={name}
+        value={value}
+        onChange={update}
+      />
+    </label>
   );
 }
-
-// const [formValues, setFormValues] = useReducer(
-//   (currentValues, newValues) => ({ ...currentValues, ...newValues }),
-//   initialValues
-// );
-
-// const { jobTitle, company, salary, jobStatus } = formValues;
-
-// function handleFormChange(e) {
-//   const { name, value } = e.target;
-//   setFormValues({ [name]: value });
-// }
-
-// console.log(initialValues)
-// console.log(formValues)
--- src/Pages/AddNew/index.js
+++ src/Pages/AddNew/index.js
@@ -3,8 +3,40 @@ import LogoutButton from "../../Components/LogoutButton";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import Input from "../../Components/Input";
+import React, { useReducer } from "react";
+
+const initialValues = {
+  jobTitle: "",
+  company: "",
+  salary: "",
+  jobStatus: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "update_input":
+      return {
+        ...state,
+        [action.key]: action.value,
+      };
+    default:
+      return state;
+  }
+}
 
 export default function AddNew() {
+  const [state, dispatch] = useReducer(reducer, initialValues);
+
+  function callDispatch(e, key) {
+    dispatch({
+      type: "update_input",
+      value: e.target.value,
+      key: key,
+    });
+  }
+
+  console.log(state);
+
   const { isLoading, isAuthenticated, logout } = useAuth0();
 
   if (!isLoading && !isAuthenticated) {
@@ -23,7 +55,36 @@ export default function AddNew() {
       </main>
       <Profile />
       <LogoutButton />
-      <Input />
+      <form>
+        <Input
+          labelText={"Job Title"}
+          type={"text"}
+          name={"jobTitle"}
+          value={state.value}
+          update={(e) => callDispatch(e, "jobTitle")}
+        />
+        <Input
+          labelText={"Company"}
+          type={"text"}
+          name={"company"}
+          value={state.value}
+          update={(e) => callDispatch(e, "company")}
+        />
+        <Input
+          labelText={"Salary"}
+          type={"text"}
+          name={"salary"}
+          value={state.value}
+          update={(e) => callDispatch(e, "salary")}
+        />
+        <Input
+          labelText={"Job Status"}
+          type={"text"}
+          name={"jobStatus"}
+          value={state.value}
+          update={(e) => callDispatch(e, "jobStatus")}
+        />
+      </form>
     </>
   ) : (
     <div>
