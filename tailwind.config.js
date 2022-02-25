1402d173ea51d3d8edebf932750d9c6eed2c2678
kattana <kattanaluz@gmail.com>
2022-02-25 10:45:48 +0000

    Added color to tailWind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
     "./public/index.html",
   ],
   theme: {
+    colors: {
+      beige: "#F4DFCC",
+      coral: "#FA9583",
+      lightBlue: "#D6EAF8",
+      mainBlue: "#21618C",
+      blueWhite: "#EBF5FB",
+      blue: "#85C1E9",
+      grey: "#7E7979",
+      black: "#0B0D17",
+      aqua: "#A2D9CE",
+    },
     extend: {},
     plugins: [require("tailwindcss"), require("autoprefixer")],
   },
