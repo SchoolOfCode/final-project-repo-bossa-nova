019250f61db16c85434d5e3110146b58efc2e855
kattana <kattanaluz@gmail.com>
2022-02-28 10:52:00 +0000

    Changed heroContainer component

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,6 +3,7 @@ import LogoutButton from "../../Components/LogoutButton";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import StickyHeadTable from "../../Components/Table";
+import HeroContainer from "../../Components/LayoutComponents/HeroContainer";
 
 export default function Home() {
   const { isLoading, isAuthenticated, logout } = useAuth0();
@@ -11,23 +12,27 @@ export default function Home() {
     logout({ returnTo: window.location.origin });
   }
 
-  return isAuthenticated ? (
-    <>
-      <nav>
-        <Link to="/add-new">Add new</Link>
-        <Link to="/resources">Resources</Link>
-        <Link to="/update">Update</Link>
-      </nav>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-      </main>
-      <Profile />
-      <LogoutButton />
-      <StickyHeadTable />
-    </>
-  ) : (
-    <div>
-      <h1>Loading...</h1>
-    </div>
+  return (
+    <HeroContainer title={"My jobs application"}>
+      {isAuthenticated ? (
+        <>
+          <nav>
+            <Link to="/add-new">Add new</Link>
+            <Link to="/resources">Resources</Link>
+            <Link to="/update">Update</Link>
+          </nav>
+          <main>
+            <h2>Welcome to the homepage!</h2>
+          </main>
+          <Profile />
+          <LogoutButton />
+          <StickyHeadTable />
+        </>
+      ) : (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      )}
+    </HeroContainer>
   );
 }
--- src/Pages/Resources/index.js
+++ src/Pages/Resources/index.js
@@ -2,6 +2,7 @@ import Profile from "../../Components/Profile";
 import LogoutButton from "../../Components/LogoutButton";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import HeroContainer from "../../Components/LayoutComponents/HeroContainer";
 
 export default function Resources() {
   const { isLoading, isAuthenticated, logout } = useAuth0();
@@ -10,22 +11,27 @@ export default function Resources() {
     logout({ returnTo: window.location.origin });
   }
 
-  return isAuthenticated ? (
-    <>
-      <nav>
-        <Link to="/add-new">Add new</Link>
-        <Link to="/home">Home</Link>
-        <Link to="/update">Update</Link>
-      </nav>
-      <main>
-        <h2>Welcome to the resources!</h2>
-      </main>
-      <Profile />
-      <LogoutButton />
-    </>
-  ) : (
-    <div>
-      <h1>Loading...</h1>
-    </div>
+  return (
+    <HeroContainer title={"Resources"}>
+      {" "}
+      {isAuthenticated ? (
+        <>
+          <nav>
+            <Link to="/add-new">Add new</Link>
+            <Link to="/home">Home</Link>
+            <Link to="/update">Update</Link>
+          </nav>
+          <main>
+            <h2>Welcome to the resources!</h2>
+          </main>
+          <Profile />
+          <LogoutButton />
+        </>
+      ) : (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      )}
+    </HeroContainer>
   );
 }
