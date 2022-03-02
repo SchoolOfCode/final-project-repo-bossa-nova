b9b255c3a9c0761997e3aa18f7c065914aecdb7c
drinobre <dri_nobre@hotmail.com>
2022-03-02 15:01:03 +0000

    fetch the initial data to update page

--- src/Pages/Update/index.js
+++ src/Pages/Update/index.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import HeroContainer from "../../Components/LayoutComponents/HeroContainer";
 import { useParams } from "react-router-dom";
+import Input from "../../Components/Input";
+// import Select from "../../Components/Select";
+import { useEffect, useState } from "react";
 
 export default function Update() {
   const { isLoading, isAuthenticated, logout } = useAuth0();
@@ -13,6 +16,22 @@ export default function Update() {
   // params from query string
 
   const { user_id, job_id } = params;
+
+  //Fetch the data
+  const [initialState, setInitialState] = useState(null);
+
+  const URL = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`${URL}/api/user/${user_id}/${job_id}`);
+      const data = await response.json();
+      setInitialState(data.payload[0]);
+    }
+    fetchData();
+  }, [URL, user_id, job_id]);
+
+  console.log(initialState);
 
   if (!isLoading && !isAuthenticated) {
     logout({ returnTo: window.location.origin });
@@ -33,6 +52,40 @@ export default function Update() {
           </main>
           <Profile />
           <LogoutButton />
+          <form>
+            <Input
+              labelText={"Job Title"}
+              type={"text"}
+              name={"jobTitle"}
+              // value={state.jobTitle}
+              // update={(e) => callDispatch(e, "jobTitle")}
+            />
+            <Input
+              labelText={"Company"}
+              type={"text"}
+              name={"company"}
+              // value={state.company}
+              // update={(e) => callDispatch(e, "company")}
+            />
+            <Input
+              labelText={"Min Salary"}
+              type={"text"}
+              name={"minSalary"}
+              // value={state.minSalary}
+              // update={(e) => callDispatch(e, "minSalary")}
+            />
+            <Input
+              labelText={"Max Salary"}
+              type={"text"}
+              name={"maxSalary"}
+              // value={state.maxSalary}
+              // update={(e) => callDispatch(e, "maxSalary")}
+            />
+            {/* <Select
+              value={state.jobStatus}
+              update={(e) => callDispatch(e, "jobStatus")}
+            /> */}
+          </form>
         </>
       ) : (
         <div>
