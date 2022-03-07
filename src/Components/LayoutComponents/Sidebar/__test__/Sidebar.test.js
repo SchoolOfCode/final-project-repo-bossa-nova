2fc83f43309330e6295d65d7e455b1ce038f5a8e
chirazzzz <chiragmehta.uk@gmail.com>
2022-03-07 09:26:53 +0000

    created describe block for sidebar tests

--- src/Components/LayoutComponents/Sidebar/__test__/Sidebar.test.js
+++ src/Components/LayoutComponents/Sidebar/__test__/Sidebar.test.js
@@ -11,27 +11,28 @@ const MockSidebar = () => {
 };
 
 /* Not convinced how valuable these visiblity tests are because I've hardcoded the display prop... */
+describe("Sidebar tests to check visibility at different screen sizes", () => {
+  it("should render mobile sidebar at small screens", () => {
+    render(<MockSidebar />);
+    const mobileSidebar = screen.getByTestId("mobile-sidebar");
+    expect(mobileSidebar).toBeVisible();
+  });
 
-it("should render mobile sidebar at small screens", () => {
-  render(<MockSidebar />);
-  const mobileSidebar = screen.getByTestId("mobile-sidebar");
-  expect(mobileSidebar).toBeVisible();
-});
+  it("should NOT render desktop sidebar at small screens", () => {
+    render(<MockSidebar className="hidden" />);
+    const mobileSidebar = screen.getByTestId("desktop-sidebar");
+    expect(mobileSidebar).not.toBeVisible();
+  });
 
-it("should NOT render desktop sidebar at small screens", () => {
-  render(<MockSidebar className="hidden" />);
-  const mobileSidebar = screen.getByTestId("desktop-sidebar");
-  expect(mobileSidebar).toBeVisible();
-});
+  it("should NOT render mobile sidebar at medium sized screens and above", () => {
+    render(<MockSidebar className="md:hidden" />);
+    const mobileSidebar = screen.getByTestId("mobile-sidebar");
+    expect(mobileSidebar).not.toBeVisible();
+  });
 
-it("should NOT render mobile sidebar at medium sized screens and above", () => {
-  render(<MockSidebar className="md:hidden" />);
-  const mobileSidebar = screen.getByTestId("mobile-sidebar");
-  expect(mobileSidebar).toBeVisible();
-});
-
-it("should render desktop sidebar at medium sized screens and above", () => {
-  render(<MockSidebar />);
-  const mobileSidebar = screen.getByTestId("desktop-sidebar");
-  expect(mobileSidebar).toBeVisible();
+  it("should render desktop sidebar at medium sized screens and above", () => {
+    render(<MockSidebar />);
+    const mobileSidebar = screen.getByTestId("desktop-sidebar");
+    expect(mobileSidebar).toBeVisible();
+  });
 });
