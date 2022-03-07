3b0f76716384741ba8b27cadd612d198e76092dd
benmhcode <85193753+benmhcode@users.noreply.github.com>
2022-03-07 12:25:21 +0000

    updated styling

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -12,18 +12,30 @@ export default function Login() {
             src="logo-01.png"
             alt="logo"
           />
-          <img
-            className="absolute z-10 h-[400px] w-[301px] hidden md:block object-cover"
-            src="login-image.png"
-            alt="login"
-          />
-          <h2 className="md:hidden">Tracker Your Jobs</h2>
-          <div className="absolute z-20 hidden md:block text-loginDesktop">
-            <h2 className="top-[10vh]">Tracker</h2>
-            <h2 className="top-[20vh] ml-40">Your</h2>
-            <h2 className="top-[30vh] ml-80">Jobs</h2>
+          <div className="absolute hidden md:block">
+            <img
+              className=" z-10  object-cover h-[400px]"
+              src="login-image.png"
+              alt="login"
+            />
+            <div className="z-30 h-[400px] w-[301px]  left-40 top-40 rounded-lg p-8 bg-coral">
+              <h4>ABOUT</h4>
+              <br />
+              <p>
+                Bossa Nova job tracker is an easy way to organise your job
+                applications.
+              </p>
+              <br />
+              <p>No more using speadsheets or email</p>
+            </div>
           </div>
-          <div className="absolute h-[400px] w-[301px] bottom-40 left-10 rounded-lg p-8 bg-coral">
+          <h2 className="md:hidden">Tracker Your Jobs</h2>
+          <div className="absolute z-40 hidden md:block text-loginDesktop ml-16 mt-10">
+            <h2 className="">Tracker</h2>
+            <h2 className=" ml-40">Your</h2>
+            <h2 className=" ml-80">Jobs</h2>
+          </div>
+          <div className="md:hidden">
             <h4>ABOUT</h4>
             <br />
             <p>
