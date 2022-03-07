aa4f52b2c075781216713cd67abfa9996703df74
kattana <kattanaluz@gmail.com>
2022-03-07 12:04:48 +0000

    form validation

--- src/Pages/AddNew/index.js
+++ src/Pages/AddNew/index.js
@@ -52,16 +52,35 @@ export default function AddNew() {
 
   const navigate = useNavigate();
 
+  //------- form validation -------//
+  function validateForm(min, max, jobTitle, company) {
+    if (min > 0 && max > min && jobTitle !== "" && company !== "") {
+      return true;
+    }
+    return false;
+  }
+
   const URL = process.env.REACT_APP_API_URL;
   async function handlePostRequest() {
-    const response = await fetch(`${URL}/api/user/${user.sub}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(state),
-    });
-    navigate("/home");
-    alert("job created");
-    return await response.json();
+    if (
+      validateForm(
+        state.minSalary,
+        state.maxSalary,
+        state.jobTitle,
+        state.company
+      )
+    ) {
+      const response = await fetch(`${URL}/api/user/${user.sub}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(state),
+      });
+      navigate("/home");
+      alert("job created");
+      return await response.json();
+    } else {
+      alert("invalid input");
+    }
   }
 
   return (
