d676f98ce79c541232406e558ea9c40687b41db4
ciisiq <cii_3@hotmail.com>
2022-03-07 14:20:40 +0000

    Input fire and button fire test

--- src/Components/Input/__test__/Input.test.js
+++ src/Components/Input/__test__/Input.test.js
@@ -1,17 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Input from "..";
 
-describe("Tests checking if input elements appear on page", () => {
+describe("input test", () => {
   it("Should render an input with the labelText", () => {
     render(<Input labelText="Company" />);
     const InputElement = screen.getByLabelText(/company/i);
     expect(InputElement).toBeInTheDocument();
   });
-  
+
   it("Should only accept number data type in input", () => {
     render(<Input labelText="Min Salary" />);
     const InputElement = screen.getByLabelText(/min Salary/i);
     expect(InputElement).toBeInTheDocument();
   });
-})
 
+  it("Should fire update when the input changed and value matches input", () => {
+    const update = jest.fn();
+    render(<Input labelText="company" update={update} />);
+    const InputElement = screen.getByLabelText(/company/i);
+    fireEvent.change(InputElement, { target: { value: "Google" } });
+    expect(update).toHaveBeenCalledTimes(1);
+    expect(InputElement.value).toBe("Google");
+  });
+});
