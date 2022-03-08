958d00879a4f279f66fd8f94769a23d3c154e019
benmhcode <85193753+benmhcode@users.noreply.github.com>
2022-03-08 14:29:44 +0000

    added mui core dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./Components/App/App.js";
 import { Auth0Provider } from "@auth0/auth0-react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
   <Auth0Provider
@@ -11,6 +12,7 @@ ReactDOM.render(
     redirectUri={`${window.location.origin}/home`}
   >
     <BrowserRouter>
+      <CssBaseline />
       <App />
     </BrowserRouter>
   </Auth0Provider>,
